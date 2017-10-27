8823da7a26ba8c8318b078219f655fe5d51fd53d
kello.tomas <kello.tomas@gmail.com>
2017-10-27 18:28:36 -0400

    read signal to dictionary

--- read_wifi.py
+++ read_wifi.py
@@ -1,3 +1,15 @@
-import os
-os.system("sudo iwlist wlp2s0 scan | grep -oP '(Address: |Signal level=)\K\S*'")
+import subprocess
+data = subprocess.check_output("sudo iwlist wlp2s0 scan | grep -oP '(Address: |Signal level=)\K\S*'", shell=True)
+#data = os.system("sudo iwlist wlp2s0 scan | grep -oP '(Address: |Signal level=)\K\S*'")
 
+wifi = dict()
+key = ""
+for line in data.splitlines():
+  if(key == ""):
+    key = line
+  else:
+    wifi[key] = line
+    key = ""
+
+for key, value in wifi.iteritems():
+  print ("SSID: " + key + " signal: " + value +    " db")
